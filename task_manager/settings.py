8599cdd298c9492893ad6128ae63423735ee8e3b
stanislavglazko <stanislavglazko@MacBook-Air-Apple.local>
2020-12-12 14:05:48 +0300

    new base

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -139,3 +139,8 @@ ROLLBAR = {
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
