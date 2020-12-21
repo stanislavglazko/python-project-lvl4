99758fea3a17a38be672a616a92c024da551a6c8
stanislavglazko <stanislavglazko@MacBook-Air-Apple.local>
2020-12-21 23:15:44 +0300

    new index

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 from django.urls import path, include
 # from django.views.generic.base import TemplateView
 from tasks.views import ListOfTasks
 
 urlpatterns = [
-    # path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('', ListOfTasks.as_view(), name='tasks'),
+    path('', RedirectView.as_view(url='tasks/')),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
